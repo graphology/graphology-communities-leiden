7068377fdcbde8625859401eaa46000f9166ce31
Yomguithereal <guillaumeplique@gmail.com>
2020-12-08 14:36:54 +0100

    Better types

--- utils.d.ts
+++ utils.d.ts
@@ -3,6 +3,10 @@ import {UndirectedLouvainIndex} from 'graphology-indices/neighborhood/louvain';
 
 type PointerArray = Uint8Array | Uint16Array | Uint32Array | Float64Array;
 
+type UndirectedLeidenAddendaOptions = {
+  rng?: () => number;
+};
+
 export class UndirectedLeidenAddenda {
   B: number;
   resolution: number;
@@ -18,6 +22,7 @@ export class UndirectedLeidenAddenda {
   neighboringCommunities: SparseMap<number>;
   cumulativeIncrement: Float64Array;
 
+  constructor(index: UndirectedLeidenAddenda, options?: UndirectedLeidenAddendaOptions);
   groupByCommunities(): void;
   mergeNodesSubset(): void;
   refinePartition(): void;
